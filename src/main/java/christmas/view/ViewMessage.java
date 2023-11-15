fa17d0b147a5fd4f15bdc5da3cf7b59d4cc0e23b
U-keun Song <sukeun319@gmail.com>
2023-11-15 15:47:43 +0900

    Refactor : 출력 메시지 수정

--- src/main/java/christmas/view/ViewMessage.java
+++ src/main/java/christmas/view/ViewMessage.java
@@ -10,22 +10,22 @@ public enum ViewMessage {
     /*
     출력 관련 메시지
      */
-    EVENT_OVERVIEW_START_MESSAGE("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리보기!"),
-    ORDER_MENU_TAG("\n<주문 메뉴>\n"),
+    EVENT_OVERVIEW_START_MESSAGE("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리보기!\n"),
+    ORDER_MENU_TAG("<주문 메뉴>\n"),
     ORDER_FORMAT("%s %d개\n"),
     TOTAL_ORDER_AMOUNT_TAG("<할인 전 총주문 금액>\n"),
-    TOTAL_ORDER_AMOUNT_FORMAT("%,d원"),
-    GIVEAWAY_MENU_TAG("\n<증정 메뉴>\n"),
-    GIVEAWAY_MENU_FORMAT("샴페인 1개"),
-    BENEFIT_SPECIFICATION_TAG("\n<혜택 내역>\n"),
+    TOTAL_ORDER_AMOUNT_FORMAT("%,d원\n"),
+    GIVEAWAY_MENU_TAG("<증정 메뉴>\n"),
+    GIVEAWAY_MENU_FORMAT("샴페인 1개\n"),
+    BENEFIT_SPECIFICATION_TAG("<혜택 내역>\n"),
     BENEFIT_SPECIFICATION_FORMAT("%s: -%,d원\n"),
     TOTAL_BENEFIT_AMOUNT_TAG("<총혜택 금액>\n"),
-    TOTAL_BENEFIT_AMOUNT_FORMAT("%,d원"),
-    TOTAL_PAYMENT_AMOUNT_TAG("\n<할인 후 예상 결제 금액>\n"),
-    TOTAL_PAYMENT_AMOUNT_FORMAT("%,d원"),
-    EVENT_BADGE_TAG("\n<12월 이벤트 배지>\n"),
+    TOTAL_BENEFIT_AMOUNT_FORMAT("%,d원\n"),
+    TOTAL_PAYMENT_AMOUNT_TAG("<할인 후 예상 결제 금액>\n"),
+    TOTAL_PAYMENT_AMOUNT_FORMAT("%,d원\n"),
+    EVENT_BADGE_TAG("<12월 이벤트 배지>\n"),
     EVENT_BADGE("%s"),
-    NONE("없음");
+    NONE("없음\n");
     private final String message;
 
     ViewMessage(String message) {
